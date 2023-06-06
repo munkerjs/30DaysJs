c5b5939d96b3c8af926c4b9a6f85c31b8d42334e
Münker Erkadam <munkererkadam@gmail.com>
2023-06-06 19:43:41 +0300

    Dizilerle ilgili çalışma

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // Dizi İşlemleri
 
 const arr = Array();
-arr.push("münker", "erkadam");
-console.log(arr.length);
-console.log(arr[1]);
+arr.push("münker", "erkadam", "adana"); // diziye eleman ekler
+console.log(arr.length); // dizi uzunluğu
+console.log(arr[1]); // dizinin 1. elemanı
+console.log(arr.splice(0, 3)); // dizinin 0 ile 3 numara arasındaki elemanları çağırır.
