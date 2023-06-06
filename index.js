0a14041a0a236453de4ba608c48689504aadb841
Münker Erkadam <munkererkadam@gmail.com>
2023-06-06 19:47:21 +0300

    Dizi elemanı değiştirme, diziyi boşaltma

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ arr.push("münker", "erkadam", "adana"); // diziye eleman ekler
 console.log(arr.length); // dizi uzunluğu
 console.log(arr[1]); // dizinin 1. elemanı
 console.log(arr.splice(0, 3)); // dizinin 0 ile 3 numara arasındaki elemanları çağırır.
+
+// Dizi Değiştirme
+const meyveler = ["elma", "armut", "karpuz", "kivi", "üzüm"];
+console.log(meyveler);
+meyveler[1] = "Kavun"; // 1. elemanı değiştirir ve yeni eleman atar.
+console.log(meyveler);
+
+// Dizi Temizleme
+meyveler = []; // dizi temizler.
+console.log(meyveler); // boş bir dizi
